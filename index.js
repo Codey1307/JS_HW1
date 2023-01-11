69b6b4ee75012d5dcf72aca4ae1498bef5f17ad5
Codey <1307>
2023-01-11 15:17:20 +0200

    hw1

---
+++ index.js
@@ -0,0 +1,17 @@
+// Завдання 1
+let admin
+let name = 'Dmytro'
+admin = name
+console.log(admin)
+// Завдання 2
+let days = 4
+let daysSecond 
+let hoursInDay = 24
+let minutesInHour = 60
+let secondsInMinute = 60
+ daysSecond = days * hoursInDay * minutesInHour * secondsInMinute
+console.log(daysSecond)
+//Завдання 3
+let result = prompt('Введіть будь-яке число:')
+console.log(result)
+
